7130689d76ad1b82f4f288e8d066d4cb77b25c48
Leonardo Trone <Leonardo Trone>
2017-04-07 16:08:53 -0300

    iniciando implementação da busca em profundidade

---
+++ src/Main/ArestaBP.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author leotr
+ */
+public class ArestaBP {
+    double peso;
+    VerticeBuscaProfundidade verticeDestino;
+    VerticeBuscaProfundidade verticeOrigem;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public VerticeBuscaProfundidade getVerticeDestino() {
+        return verticeDestino;
+    }
+
+    public void setVerticeDestino(VerticeBuscaProfundidade verticeDestino) {
+        this.verticeDestino = verticeDestino;
+    }
+
+    public VerticeBuscaProfundidade getVerticeOrigem() {
+        return verticeOrigem;
+    }
+
+    public void setVerticeOrigem(VerticeBuscaProfundidade verticeOrigem) {
+        this.verticeOrigem = verticeOrigem;
+    }
+    
+    
+}
---
+++ src/Main/VerticeBuscaProfundidade.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author leotr
+ */
+public class VerticeBuscaProfundidade extends VerticeBuscaLargura {
+     Double f;
+    public VerticeBuscaProfundidade(String id) {
+        super(id);
+    }
+
+    public Double getF() {
+        return f;
+    }
+
+    public void setF(Double f) {
+        this.f = f;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Color getCor() {
+        return cor;
+    }
+
+    public void setCor(Color cor) {
+        this.cor = cor;
+    }
+
+    public Double getD() {
+        return d;
+    }
+
+    public void setD(Double d) {
+        this.d = d;
+    }
+
+    public VerticeBuscaLargura getPi() {
+        return pi;
+    }
+
+    public void setPi(VerticeBuscaLargura pi) {
+        this.pi = pi;
+    }
+     
+}
